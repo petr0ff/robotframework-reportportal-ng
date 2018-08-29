8dbfa2540f9ef28591ce70e4ec5f24e07fe2a239
German_Petrov <German_Petrov@epam.com>
2018-08-29 14:01:00 +0300

    Update skip list for logging

--- reportportal_listener/__init__.py
+++ reportportal_listener/__init__.py
@@ -212,6 +212,9 @@ class reportportal_listener(object):  # noqa
                         output=', '.join(attributes['assign'])) if attributes['assign'] else ""
                     input_data_string = " [Input data] {input}".format(
                         input=', '.join(attributes['args'])) if attributes['args'] else ""
+                    if "." in name:
+                        name = name.split(".")[-1]
+
                     message = {
                         "message": u"[{type}] {name}{input}{output}".format(
                             type=type,
