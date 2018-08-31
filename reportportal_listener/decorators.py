1f93f68f9b078b647f62182d74ed346b8482cb3f
German_Petrov <German_Petrov@epam.com>
2018-08-31 17:17:43 +0300

    Debug

--- reportportal_listener/decorators.py
+++ reportportal_listener/decorators.py
@@ -17,7 +17,7 @@ def retry(exceptions_to_check, wait=2, retries=3):
             attempts = retries + 1
             for attempt in range(attempts):
                 try:
-                    BuiltIn().log_to_console("TRYING %d" % attempt)
+                    BuiltIn().log_to_console("TRYING %d of %d",  attempt, attempts)
                     return f(*args, **kwargs)
                 except exceptions_to_check as e:
                     BuiltIn().log_to_console("GOT EXCEPTION: %s", e)
